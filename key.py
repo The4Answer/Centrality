f0c04011b7c3e945e3d4e30dc1dd73e2ea0b8264
The4Answer <67497475+The4Answer@users.noreply.github.com>
2022-11-28 20:32:28 +0300

    Create key.py

---
+++ key.py
@@ -0,0 +1,4 @@
+#Link to get a token
+#https://oauth.vk.com/oauth/authorize?client_id=51451276&scope=friends&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1&slogin_h=3a2968973111406503.ec15ab45b1ae6414eb&__q_hash=83c8c07281fee245805c4440dfc7f54e#
+
+API_KEY_FRIENDS = "your key"
